ee9d6ca114d2a17a13a229dbf9c478894085e19c
Epinder <epinder.singh@gmail.com>
2020-08-10 16:58:56 +0800

    Creating notes app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,54 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
-console.log(msg);
+//Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (argv) {
+    console.log(chalk.green(`Title: ${argv.title}`));
+    console.log(chalk.yellow(`Body: ${argv.body}`));
+  },
+});
 
-console.log(chalk.red.inverse.bold('Success..'));
+// Create Remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note!',
+  handler: function () {
+    console.log('Removing a note!');
+  },
+});
+
+//Create list notes
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function () {
+    console.log('Listing out all notes!');
+  },
+});
+
+// Create read notes
+yargs.command({
+  command: 'read',
+  describe: 'Read a note!',
+  handler: function () {
+    console.log('Reading a note!');
+  },
+});
+
+yargs.parse();
