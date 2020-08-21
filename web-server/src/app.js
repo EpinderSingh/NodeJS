38b437e35874a9398f1a54d1033980bc49a5e59c
Epinder <epinder.singh@gmail.com>
2020-08-21 10:16:12 +0800

    404 page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handelars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirPath));
@@ -32,6 +35,7 @@ app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Help',
     helpText: 'This is some helpful text.',
+    name: 'Epinder Mann',
   });
 });
 
@@ -39,6 +43,22 @@ app.get('/weather', (req, res) => {
   res.send({
     location: 'Perth',
     forecast: 'It is 14 Degree. High chance of rain.',
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Epinder Mann',
+    errorMessage: 'Help artical not found',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMessage: 'Page not found.',
+    name: 'Epinder Mann',
   });
 });
 
