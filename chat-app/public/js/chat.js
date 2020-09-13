f37f8da62f91dae97237e63838941448d45b60f8
Epinder <epinder.singh@gmail.com>
2020-09-13 16:40:24 +0800

    complete chat-app version

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -12,28 +12,64 @@ const messageTemplate = document.querySelector('#message-template').innerHTML;
 const locationMessageTemplate = document.querySelector(
   '#location-message-template',
 ).innerHTML;
+const sideBarTemplate = document.querySelector('#sidebar-template').innerHTML;
 
 // Options
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
+const autoscroll = () => {
+  // New message element
+  const $newMessage = $messages.lastElementChild;
+
+  // Height of the new message
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  // visible Height
+  const visibleHeight = $messages.offsetHeight;
+
+  // Height of messages container
+  const containerHeight = $messages.scrollHeight;
+
+  // How far have i scroll
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
+
 socket.on('message', (message) => {
-  console.log(message);
+  // console.log(message);
   const html = Mustache.render(messageTemplate, {
+    username: message.username,
     message: message.text,
     createdAt: moment(message.createdAt).format('h:mm a'),
   });
   $messages.insertAdjacentHTML('beforeend', html);
+  autoscroll();
 });
 
 socket.on('locationMessage', (message) => {
-  console.log(message);
+  // console.log(message);
   const html = Mustache.render(locationMessageTemplate, {
+    username: message.username,
     url: message.url,
     createdAt: moment(message.createdAt).format('h:mm a'),
   });
   $messages.insertAdjacentHTML('beforeend', html);
+  autoscroll();
+});
+
+socket.on('roomData', ({ room, users }) => {
+  const html = Mustache.render(sideBarTemplate, {
+    room,
+    users,
+  });
+  document.querySelector('#sidebar').innerHTML = html;
 });
 
 $messageForm.addEventListener('submit', (e) => {
@@ -52,7 +88,7 @@ $messageForm.addEventListener('submit', (e) => {
     if (error) {
       return console.log(error);
     }
-    console.log('Message Delivered! ');
+    // console.log('Message Delivered! ');
   });
 });
 
@@ -71,7 +107,7 @@ $sendLocationButton.addEventListener('click', () => {
         longitude: position.coords.longitude,
       },
       () => {
-        console.log('Location shared!');
+        // console.log('Location shared!');
         $sendLocationButton.removeAttribute('disabled');
       },
     );
